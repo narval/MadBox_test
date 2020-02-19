64d15a0bb6dee23fbb81de245727559136d3eaf8
Isaac <isaaclpe@gmail.com>
2020-02-19 17:43:32 +0100

    Adds functionality to start game button in main menu so it loads the MainScene

---
+++ MadBoxTest/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private MainMenuUIController mainMenuUIController;
+    [SerializeField] private string gameSceneName;
+
+    void Start()
+    {
+        mainMenuUIController.ButtonClickedRegister(StartGame);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(gameSceneName);
+    }
+}
---
+++ MadBoxTest/Assets/Scripts/MainMenuUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIController : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    public delegate void ButtonClicked();
+    public event ButtonClicked ButtonClickedEvent;
+
+    public void Start()
+    {
+        startButton.onClick.AddListener(OnStartPressed);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClickedRegister(ButtonClicked startCallback)
+    {
+        ButtonClickedEvent += startCallback;
+    }
+
+    public void OnStartPressed()
+    {
+        ButtonClickedEvent.Invoke();
+    }
+}
