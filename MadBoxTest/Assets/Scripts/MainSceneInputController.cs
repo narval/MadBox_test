b63efdb2a09c423db1308cb9c901bb4bcf8dbd09
Isaac <isaaclpe@gmail.com>
2020-02-19 18:01:15 +0100

    - Imports image references. - Creates MainSceneUI and some controllers.

---
+++ MadBoxTest/Assets/Scripts/MainSceneInputController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneInputController : MonoBehaviour
+{
+    [SerializeField] private MainMenuUIController mainMenuUIController;
+
+    private bool canRun;
+
+    void Start()
+    {
+        mainMenuUIController.ButtonClickedRegister(RunButtonPressed);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void RunButtonPressed()
+    {
+        if (canRun)
+        {
+
+        }
+        //Run
+    }
+}
---
+++ MadBoxTest/Assets/Scripts/MainSceneUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneUIController : MonoBehaviour
+{
+    [SerializeField] private Button runButton;
+
+    public delegate void ButtonClicked();
+    public event ButtonClicked ButtonClickedEvent;
+
+    public void Start()
+    {
+        runButton.onClick.AddListener(OnStartPressed);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClickedRegister(ButtonClicked startCallback)
+    {
+        ButtonClickedEvent += startCallback;
+    }
+
+    public void OnStartPressed()
+    {
+        ButtonClickedEvent.Invoke();
+    }
+}
